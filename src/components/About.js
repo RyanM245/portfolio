1d1b0044cf55b3aeea33c394c58d8c8fd0cdf9bf
Ryan Milne <ryanmilne101@gmail.com>
2020-10-19 17:08:21 -0600

    more in about us

--- src/components/About.js
+++ src/components/About.js
@@ -7,45 +7,45 @@ const About = () => {
         <div className="about-me">
           <h1>ABOUT ME</h1>
           <p>
-            Hi, I'm Keaton Braithwaite! I'm a software developer who
-            loves to create and design websites and applications that are
-            meaningful to people. Coming from a sales background and having
-            helped build a pool construction company from the ground up I know
-            how to work and am eager to learn. I am also a father of 3 beautiful
-            children and husband to an amazing wife. I love the mountains here
-            in Utah and exploring the many areas to hike. I'm a big sports
-            enthusiast and enjoy playing football, basketball , pickleball and
-            more.
+          Hey, I'm Ryan Milne! I am a web developer and I love to push myself in every project I work on! Although I am fairly new to the Web Development world I have grown up with a love to solve problems. I grew up in a small town out in the middle of nowhere Utah. This town and surrounding area is a place I love and is where I learned the importance of hard work and also learned how to be self-sufficient.  
           </p>
         </div>
         <div className="experience">
           <div className="devmtn">
-            <h1 className="fas fa-code"><span>DEVMOUNTAIN GRADUATE</span></h1>
+            <h1 className="fas fa-code">
+              <span>DEVMOUNTAIN - HONORS GRADUATE</span>
+            </h1>
             <p>
               Immersive, 13-week web-development bootcamp for learning
               Javascript, HTML/CSS, React, Node, and PostgreSQL frameworks
             </p>
           </div>
           <div className="premier">
-            <h1 className="fas fa-swimming-pool"><span>PREMIER POOLS SALES & DESIGN</span></h1>
+            <h1 className="fas fa-coins">
+              <span>APTIVE ENVIRONMENTAL - ROUTE MANAGER </span>
+            </h1>
             <p>
-              Helped build this pool construction company from the ground up and
-              got us to over $2.5 million in sales and still growing
+              Worked daily as a team to accomplish goals that pushed us. Used
+              software called PestRoutes to schedule and manage customers and
+              technicians.
             </p>
           </div>
           <div className="franz">
-            <h1 className="fas fa-bread-slice"><span>ROUTE SALES AT FRANZ BAKERY</span></h1>
+            <h1 className="fas fa-toolbox">
+              <span>LIFETIME PRODUCTS - INSTALL TEAM LEAD</span>
+            </h1>
             <p>
-              Sold to national and local grocery stores and lead the state in
-              summer bun sales in 2015
+              Lead two teams of four in managing installation of outdoor
+              equipment across 3 states
             </p>
           </div>
           <div className="crane">
-            <h1 className="fas fa-swimmer"><span>SERVICE MANAGER AT CRANE POOL CO</span></h1>
+            <h1 className="fas fa-tools">
+              <span>HOLEY DRYWALL & REPAIR - OWNERS ASSISTANT</span>
+            </h1>
             <p>
-              Helped build our service side of the company coming out of the
-              recession and grew clientele over 40% over the two years I managed
-              it
+              Worked side by side with the owner of the company to assist him
+              with increased business demands in drywall and repair services
             </p>
           </div>
         </div>
