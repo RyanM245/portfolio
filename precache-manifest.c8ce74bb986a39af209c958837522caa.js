3a9bd7624824ae3a83f72a12bd016c4e0a03cc8d
Ryan Milne <ryanmilne101@gmail.com>
2020-10-21 14:09:45 -0600

    Updates

---
+++ precache-manifest.c8ce74bb986a39af209c958837522caa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce4de0aa351ca5ae75d461f8107b00d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ef872e6f4ad0e34b244",
+    "url": "/static/css/main.6fb04233.chunk.css"
+  },
+  {
+    "revision": "4975cabd2b3ea5f15c2e",
+    "url": "/static/js/2.9ddf0155.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9ddf0155.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ef872e6f4ad0e34b244",
+    "url": "/static/js/main.a8dac79a.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "c6c864c0500a53d73f5c011bcb363e92",
+    "url": "/static/media/portfolioback.c6c864c0.jpg"
+  }
+]);
